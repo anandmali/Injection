c2d75e6b4889c0691cf54fd8a7c81b597ac7b96a
Anand M <anandm475@gmail.com>
2021-04-01 18:03:36 +0530

    added retrofit depedecies and network module

--- app/src/main/java/com/anandm/injection/BaseApplication.kt
+++ app/src/main/java/com/anandm/injection/BaseApplication.kt
@@ -6,7 +6,6 @@ import dagger.android.support.DaggerApplication
 
 class BaseApplication : DaggerApplication() {
 
-
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.builder().application(this).build()
     }
--- app/src/main/java/com/anandm/injection/MainActivity.kt
+++ app/src/main/java/com/anandm/injection/MainActivity.kt
@@ -2,19 +2,12 @@ package com.anandm.injection
 
 import android.os.Bundle
 import dagger.android.support.DaggerAppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity() {
-
-    @Inject
-    lateinit var someValue: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        textView.text = someValue
 
     }
 }
