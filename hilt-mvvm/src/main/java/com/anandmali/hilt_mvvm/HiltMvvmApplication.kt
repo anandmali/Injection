ef0b02e7c38c45c97366dbcc02bdf7a1ac41ef38
anandmali <anandm475@gmail.com>
2021-06-01 21:00:48 +0530

    annotating hilt application

---
+++ hilt-mvvm/src/main/java/com/anandmali/hilt_mvvm/HiltMvvmApplication.kt
@@ -0,0 +1,7 @@
+package com.anandmali.hilt_mvvm
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltMvvmApplication : Application()
---
+++ hilt-mvvm/src/main/java/com/anandmali/hilt_mvvm/search/SearchActivity.kt
@@ -0,0 +1,16 @@
+package com.anandmali.hilt_mvvm.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anandmali.hilt_mvvm.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+
+}
